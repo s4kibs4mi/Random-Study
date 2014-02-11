d2e08424ab6b910e99a05809802495a575312371
Sakib Sami <s4kibs4mi@gmail.com>
2014-02-12 04:12:45 +0700

    Create GDC.cpp

---
+++ GDC.cpp
@@ -0,0 +1,18 @@
+# include <stdio.h>
+# include <stdlib.h>
+int main() 
+{ 
+	int n1,n2,gcd; 
+	printf("Enter the two numbers : ") ; 
+	scanf("%d %d", &n1, &n2) ;  
+	while(n1 != n2) 
+	{ 
+		if(n1 > n2) 
+		n1 = n1 - n2 ; 
+		if(n2 > n1) 
+		n2 = n2 - n1 ; 
+	} 
+	gcd = n1 ; 
+	printf("\nThe GCD is : %d",gcd);
+	return 0;
+}
