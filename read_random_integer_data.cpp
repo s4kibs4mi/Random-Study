55bdb07d45735d07455bc56afe4facd33a93cb9c
Sakib Sami <s4kibs4mi@gmail.com>
2014-07-04 18:18:38 +0600

    Create read_random_integer_data.cpp

---
+++ read_random_integer_data.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+
+using namespace std;
+
+int main(){
+    // freopen("input.txt","r",stdin);
+    int n,i = 0;
+    while(scanf("%d",&n) == 1){ // CTRL + X to terminate loop
+        i++;
+    }
+    cout << i << endl;
+    return 0;
+}
